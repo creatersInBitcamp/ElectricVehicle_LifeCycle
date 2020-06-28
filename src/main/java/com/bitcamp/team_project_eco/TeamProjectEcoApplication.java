3f6581f1445e79becae992329db5982d13678925
LeeHyengTae <tedd911@icloud.com>
2020-06-28 22:10:19 +0900

    team project start

---
+++ src/main/java/com/bitcamp/team_project_eco/TeamProjectEcoApplication.java
@@ -0,0 +1,13 @@
+package com.bitcamp.team_project_eco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamProjectEcoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamProjectEcoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bitcamp/team_project_eco/TeamProjectEcoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitcamp.team_project_eco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProjectEcoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
