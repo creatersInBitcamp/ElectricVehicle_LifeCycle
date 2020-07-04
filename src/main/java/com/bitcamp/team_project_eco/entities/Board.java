ef3a341f101c8124cad197a25533cb02efca206d
LeeHyengTae <tedd911@icloud.com>
2020-07-04 19:26:44 +0900

    board entity
    19:26 게시판 엔티티 구성

--- src/main/java/com/bitcamp/team_project_eco/entities/Board.java
+++ src/main/java/com/bitcamp/team_project_eco/entities/Board.java
@@ -1,14 +1,25 @@
 package com.bitcamp.team_project_eco.entities;
 
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 //@Entity
+@Data
 public class Board {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long boardNo;
-    private String titie, writer, date, like;
+    private String titie;
+    private String content;
+    private String hashTag;
+    private String createDate;
+    private String updateDate;
+    private Long recomendation;
+    private Long boardHits;
+    private String userId;
+    private Long communityNo;
 }
