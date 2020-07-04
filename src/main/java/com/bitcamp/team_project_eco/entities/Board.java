b1ee94adbfbb8d2aa315ca03aa42eda2bbdc1591
박주선 <62981020+parkjusun@users.noreply.github.com>
2020-07-04 18:18:34 +0900

    Revert "17:27"
    This reverts commit a4ce5550cdce1ca0eb1e1f21df065d40b74b2bd0.

--- src/main/java/com/bitcamp/team_project_eco/entities/Board.java
+++ src/main/java/com/bitcamp/team_project_eco/entities/Board.java
@@ -2,13 +2,11 @@ package com.bitcamp.team_project_eco.entities;
 
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-//@Entity
+@Entity
 public class Board {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long boardNo;
+    @Id
+    private int boardNo;
     private String titie, writer, date, like;
 }
