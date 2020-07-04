450c6f25c407ffe5397a0e5b3d29e23867f6c069
LeeHyengTae <tedd911@icloud.com>
2020-07-04 18:31:49 +0900

    board test
    18:31

--- src/main/java/com/bitcamp/team_project_eco/controllers/BoardController.java
+++ src/main/java/com/bitcamp/team_project_eco/controllers/BoardController.java
@@ -1,4 +1,9 @@
 package com.bitcamp.team_project_eco.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/board")
 public class BoardController {
 }
