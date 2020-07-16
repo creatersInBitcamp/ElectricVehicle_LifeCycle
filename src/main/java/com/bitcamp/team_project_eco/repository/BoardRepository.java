208a216274b5363bd5111a568e7f743f212c6e81
LeeHyengTae <tedd911@icloud.com>
2020-07-16 11:55:09 +0900

    repush
    fixed

--- src/main/java/com/bitcamp/team_project_eco/repository/BoardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bitcamp.team_project_eco.repository;
-
-import com.bitcamp.team_project_eco.entities.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BoardRepository extends JpaRepository<Board, Long> {
-}
