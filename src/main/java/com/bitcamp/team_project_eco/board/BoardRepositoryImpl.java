fc742747932abb4b98204b3d3d85bf735df21067
LeeHyengTae <tedd911@icloud.com>
2020-07-17 00:03:17 +0900

    Update BoardRepositoryImpl.java
    00:03

--- src/main/java/com/bitcamp/team_project_eco/board/BoardRepositoryImpl.java
+++ src/main/java/com/bitcamp/team_project_eco/board/BoardRepositoryImpl.java
@@ -2,9 +2,12 @@ package com.bitcamp.team_project_eco.board;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+import org.springframework.stereotype.Repository;
+
+@Repository
+interface BoardRepository extends JpaRepository<Board, Long>, BoardService { }
 
 interface BoardService { }
-interface BoardRepository extends JpaRepository<Board, Long>, BoardService { }
 
 public class BoardRepositoryImpl extends QuerydslRepositorySupport implements BoardService {
 
