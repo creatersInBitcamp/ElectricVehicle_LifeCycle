b33f41d3eb114854104fb85375c70a0935eda04d
LeeHyengTae <tedd911@icloud.com>
2020-07-17 17:20:36 +0900

    Update schema.sql
    17:20

--- src/main/resources/static/schema.sql
+++ src/main/resources/static/schema.sql
@@ -43,7 +43,6 @@ CREATE TABLE residual_rate
     `five_years`     INT            NULL
 )default character set utf8 collate UTF8_GENERAL_CI;
 
-
 -- sights Table Create SQL
 CREATE TABLE sights
 (
@@ -111,4 +110,72 @@ ALTER TABLE bookmark
     ADD CONSTRAINT FK_bookmark_sights_id_sights_sights_id FOREIGN KEY (sights_id)
         REFERENCES sights (sights_id) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
+CREATE TABLE insurance
+(
+    `insurance_product`      INT            NOT NULL    AUTO_INCREMENT,
+    `carprice`               VARCHAR(45)    NULL,
+    `residential_area`       VARCHAR(45)    NULL,
+    `sortation`              VARCHAR(45)    NULL,
+    `fuel_efficiency`        VARCHAR(45)    NULL,
+    `acquisition_tax`        VARCHAR(45)    NULL,
+    `public_bond`            VARCHAR(45)    NULL,
+    `purchase_rate`          VARCHAR(45)    NULL,
+    `rate_discount`          VARCHAR(45)    NULL,
+    `discount_public_bonds`  VARCHAR(45)    NULL,
+    `additional_amount`      VARCHAR(45)    NULL,
+    `order_id`               VARCHAR(45)    NULL,
+    PRIMARY KEY (insurance_product)
+);
+
+
+-- insurance Table Create SQL
+CREATE TABLE purchase
+(
+    `order_id`           INT            NOT NULL    AUTO_INCREMENT,
+    `purchasing_method`  VARCHAR(45)    NULL,
+    `purchase_time`      VARCHAR(45)    NULL,
+    `purchase_amount`    VARCHAR(45)    NULL,
+    `car_id`             INT            NULL,
+    `user_id`            VARCHAR(45)    NULL,
+    PRIMARY KEY (order_id)
+);
+
+ALTER TABLE purchase
+    ADD CONSTRAINT FK_purchase_order_id_insurance_order_id FOREIGN KEY (order_id)
+        REFERENCES insurance (order_id) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+
+-- insurance Table Create SQL
+CREATE TABLE registration
+(
+    `registration_cost`           INT            NOT NULL    AUTO_INCREMENT,
+    `insurance_start_date`        VARCHAR(45)    NULL,
+    `insurance_termination_date`  VARCHAR(45)    NULL,
+    `manufacturer`                VARCHAR(45)    NULL,
+    `car_name`                    VARCHAR(45)    NULL,
+    `registration_year`           VARCHAR(45)    NULL,
+    `detailed_car_name`           VARCHAR(45)    NULL,
+    `detailed_list`               VARCHAR(45)    NULL,
+    `expected_insurance`          VARCHAR(45)    NULL,
+    `insurance_link`              VARCHAR(45)    NULL,
+    PRIMARY KEY (registration_cost)
+);
+
+
+-- insurance Table Create SQL
+CREATE TABLE comparison
+(
+    `car_id`              INT            NOT NULL    AUTO_INCREMENT,
+    `manufacturer`        VARCHAR(45)    NULL,
+    `manufacturing_year`  VARCHAR(45)    NULL,
+    `price`               VARCHAR(45)    NULL,
+    `fuel_efficiency`     VARCHAR(45)    NULL,
+    `cc`                  VARCHAR(45)    NULL,
+    `mile_age`            VARCHAR(45)    NULL,
+    `make`                VARCHAR(45)    NULL,
+    `picture`             VARCHAR(45)    NULL,
+    `order_id`            VARCHAR(45)    NULL,
+    PRIMARY KEY (car_id)
+);
+>>>>>>> newcar
 
