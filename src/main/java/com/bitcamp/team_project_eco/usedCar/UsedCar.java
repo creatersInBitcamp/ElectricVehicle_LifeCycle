d3ea8bddef4ffcd9593435c825e0dfc64f850020
KIMSUBIN <xxxia@naver.com>
2020-08-08 17:27:48 +0900

    create usedCar dir
    200808 17:27

---
+++ src/main/java/com/bitcamp/team_project_eco/usedCar/UsedCar.java
@@ -0,0 +1,30 @@
+package com.bitcamp.team_project_eco.usedCar;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Getter @Setter @ToString @NoArgsConstructor
+@Table(name = "used_car")
+public class UsedCar {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "usedcar_id") private Long usedCarId;
+
+    @Column(name = "price") private String price;
+
+    @Column(name = "age") private String age;
+
+    @Column(name = "mileage") private String mileage;
+
+    @Builder
+    public UsedCar(String price,
+                   String age,
+                   String mileage){
+        this.price = price;
+        this.age = age;
+        this.mileage = mileage;
+    }
+}
---
+++ src/main/java/com/bitcamp/team_project_eco/usedCar/UsedCarRepository.java
@@ -0,0 +1,6 @@
+package com.bitcamp.team_project_eco.usedCar;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsedCarRepository extends JpaRepository<UsedCar,Long>, IUsedCarRepository {
+}
