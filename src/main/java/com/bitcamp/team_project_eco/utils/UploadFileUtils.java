db3d83aa89a195ebfd0931682a9feadf32de8c4a
곽경열 <62980973+kwakky1@users.noreply.github.com>
2020-08-19 23:44:31 +0900

    S3로 넘기고 패스 설정 변경, 100MB까지 전송가능, 1MB이상 크기의 파일 전송시 yml에 servlet:     multipart:       max-file-size: 100MB       max-request-size: 100MB
          이렇게 파일 사이즈 적어줘야함, s3 key값 빼놓음, tset할때 다시 넣어야함 끝.

--- src/main/java/com/bitcamp/team_project_eco/utils/UploadFileUtils.java
+++ src/main/java/com/bitcamp/team_project_eco/utils/UploadFileUtils.java
@@ -19,11 +19,13 @@ public class UploadFileUtils {
         UUID uid = UUID.randomUUID();
 
         //savedName : 570d570a-7af1-4afe-8ed5-391d660084b7_g.JPG 같은 형식으로 만들어준다.
-        String savedName = "/"+uid.toString() + "_" + originalName;
+        //String savedName = "/"+uid.toString() + "_" + originalName;
+        String savedName = "/"+ originalName;
 
         logger.info("업로드 경로 : "+uploadPath);
         //\2017\12\27 같은 형태로 저장해준다.
-        String savedPath = calcPath(uploadPath);
+        //String savedPath = calcPath(uploadPath);
+        String savedPath = "";
 
         String uploadedFileName = null;
 
