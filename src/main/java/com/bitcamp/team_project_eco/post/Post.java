453d775f4b7ba929caf2e3729dc00f0c1cc78423
LeeHyengTae <tedd911@icloud.com>
2020-08-24 10:52:32 +0900

    Update Post.java

--- src/main/java/com/bitcamp/team_project_eco/post/Post.java
+++ src/main/java/com/bitcamp/team_project_eco/post/Post.java
@@ -4,9 +4,6 @@ import com.bitcamp.team_project_eco.comment.Comment;
 import com.bitcamp.team_project_eco.user.User;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
-
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
